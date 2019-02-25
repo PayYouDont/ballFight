e45b5d580e653fb2c34dfd92714ee549595d129c
PayYouDont <471113255@qq.com>
2019-02-25 17:57:44 +0800

    新增限制移动超出视窗边界

--- assets/scripts/ball.js
+++ assets/scripts/ball.js
@@ -18,14 +18,28 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+        this.setMoveAction();
+    },
+    setMoveAction:function(){
         this.node.on(cc.Node.EventType.TOUCH_MOVE, function (event) {
             this.opacity = 255;
             var delta = event.touch.getDelta();
             this.x += delta.x;
             this.y += delta.y;
+            cc.log(this.width,this.height)
+            if ( this.x > this.parent.width/2-this.width/2) {
+                this.x = this.parent.width/2-this.width/2;
+            } else if (this.x < -this.parent.width/2+this.width/2) {
+                this.x = -this.parent.width/2+this.width/2;
+            }
+            cc.log(this.parent.height)
+            if(this.y > this.parent.height/2-this.height/2){
+                this.y = this.parent.height/2-this.height/2;
+            }else if (this.y < -this.parent.height/2+this.height/2) {
+                this.y = -this.parent.height/2+this.height/2;
+            }
         }, this.node);
     },
-
     start () {
 
     },
